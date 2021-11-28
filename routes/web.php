f3d9d2791d61e7bee0488a17c64d757c52df5d62
nediaputrii <50162029+NediaPutrii@users.noreply.github.com>
2021-11-28 11:29:13 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ $router->get('/', function () use ($router) {
 });
 
 $router->get('/travel_agent', function () use ($router) {
-    $results = app('db')->select("SELECT * FROM travel_agent");
+    $results = app('db')->select("SELECT * FROM travel_agents");
     return response()->json($results);
 });
 
